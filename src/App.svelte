530bc5f8cc7f2c1d1d4f5b27af0574eb8fba38dd
KdenNijs <katinkadennijs@gmail.com>
2024-11-26 21:27:38 +0100

    Created accordion effect =)

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,49 @@
 <script>
-	export let name;
+	import Question from './Question.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div class='FAQblock'>
+		<h1>FAQs</h1>
+		<Question question="What is Frontend Mentor, and how will it help me?">
+			<p>Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.</p>
+		</Question>
+		<Question question="Is Frontend Mentor free?">
+			<p>Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.</p>
+		</Question>
+		<Question question="Can I use Frontend Mentor projects in my portfolio?">
+			<p>Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!</p>
+		</Question>
+		<Question question="How can I get help if I'm stuck on a challenge?">
+			<p>The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.</p>
+		</Question>
+	</div>
 </main>
 
 <style>
-	main {
+	/* main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
+	} */
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
 		font-size: 4em;
-		font-weight: 100;
+		font-weight: bold;
 	}
 
-	@media (min-width: 640px) {
+	/* @media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
+	} */
+
+	.FAQblock {
+		padding: 10px;
+		max-width: 500px;
+		border-radius: 15px;
+		margin: 20vh auto;
+		background-color: white;
 	}
 </style>
---
+++ src/Question.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let question = "Specify question";
+    let textVisible = false;
+
+    const showAnswer = () => {
+        textVisible = !textVisible;
+    }
+</script>
+
+<div>
+    <div on:click={showAnswer}>
+        <h4>{question}</h4>
+    </div>
+    {#if textVisible}
+        <slot></slot>
+    {/if}
+</div>
+
+<style>
+
+</style>
